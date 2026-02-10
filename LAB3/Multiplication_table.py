8651c0d5484572d1bbbecee4a8e1858794af6793
cs0787 <chetanshakhapure@gmail.com>
2026-02-10 14:11:21 +0530

    Update and rename table.py to Multiplication_table.py

---
+++ LAB3/Multiplication_table.py
@@ -0,0 +1,6 @@
+n = int(input("Enter number to print table:")) #user input
+
+for i in range(1,11): #loop
+    print(f"{n}x{i} = {n*i}") #table printing
+    
+
--- LAB3/table.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter number to print table:"))
-
-for i in range(1,11):
-    print(f"{n}x{i} = {n*i}")
-    
